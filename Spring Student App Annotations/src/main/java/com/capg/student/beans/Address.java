3b24437c71d0c54f72d37506fb883d9b03844400
Bharath-Surya <84221066+Bharath-Surya@users.noreply.github.com>
2021-05-15 19:07:49 +0530

    Add files via upload

---
+++ Spring Student App Annotations/src/main/java/com/capg/student/beans/Address.java
@@ -0,0 +1,83 @@
+package com.capg.student.beans;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Address {
+
+	@Value("H1")
+	private String houseNumber;
+	
+	@Value("Street 504")
+	private String streetName;
+	
+	@Value("Kerala")
+	private String stateName;
+	
+	@Value("India")
+	private String countryName;
+
+	
+	public Address() {
+		super();
+		
+	}
+
+
+	public Address(String houseNumber, String streetName, String stateName, String countryName) {
+		super();
+		this.houseNumber = houseNumber;
+		this.streetName = streetName;
+		this.stateName = stateName;
+		this.countryName = countryName;
+	}
+
+
+	public String getHouseNumber() {
+		return houseNumber;
+	}
+
+
+	public void setHouseNumber(String houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+
+	public String getStateName() {
+		return stateName;
+	}
+
+
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+
+
+	public String getCountryName() {
+		return countryName;
+	}
+
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Address [houseNumber=" + houseNumber + ", streetName=" + streetName + ", stateName=" + stateName
+				+ ", countryName=" + countryName + "]";
+	}
+	
+
+}
